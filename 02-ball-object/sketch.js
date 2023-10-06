e754088f5fed0f061ab84db920c9c3580fc4d80e
unevenlogic <32986800+unevenlogic@users.noreply.github.com>
2023-10-06 10:41:30 -0600

    Refactored 02-ball-object
    - Added mouse press movement
    - Added spawn and space bar randomization
    - Fixed and refactored border movement through modular arithmetic

--- 02-ball-object/sketch.js
+++ 02-ball-object/sketch.js
@@ -1,34 +1,49 @@
 // Ball Object Notation Demo
 // Oct. 5, 2023
 
-const spring_coeff = 0.01;
+const mouseScaling = 0.05;
+const minRad = 15;
+const maxRad = 30;
+const maxDirSpeed = 5;
 
-let theBall = {
-  x: 100,
-  y: 100,
-  r: 25,
-  rcol: 255,
-  gcol: 0,
-  bcol: 0,
-  dx: 4,
-  dy: 3,
-};
+let theBall;
+
+function spawnBall() {
+  let theBall = {
+    x: random(width),
+    y: random(height),
+    r: random(minRad, maxRad),
+    rcol: random(255),
+    gcol: random(255),
+    bcol: random(255),
+    dx: random(-maxDirSpeed, maxDirSpeed),
+    dy: random(-maxDirSpeed, maxDirSpeed),
+  };
+  return theBall;
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theBall = spawnBall();
 }
 
-function draw() {
-  background(220);
-  moveBall();
-  displayBall();
+function betterMod(n, m) {
+  let result = n % m;
+  if(result < 0) {
+    result += m;
+  }
+  return result;
+}
+
+function borderWrap(coord, r, maxSize) {
+  return betterMod(coord + r, maxSize + 2*r) - r;
 }
 
 function moveBall() {
   theBall.x += theBall.dx;
   theBall.y += theBall.dy;
-  theBall.x = (theBall.x + theBall.r) % (windowWidth + theBall.r * 2) - theBall.r;
-  theBall.y = (theBall.y + theBall.r) % (windowHeight + theBall.r * 2) - theBall.r;
+  theBall.x = borderWrap(theBall.x, theBall.r, windowWidth);
+  theBall.y = borderWrap(theBall.y, theBall.r, windowHeight);
   // theBall.dx += (width/2 - theBall.x) * spring_coeff;
   // theBall.dy += (height/2 - theBall.y) * spring_coeff;
 }
@@ -36,4 +51,29 @@ function moveBall() {
 function displayBall() {
   fill(theBall.rcol, theBall.gcol, theBall.bcol);
   circle(theBall.x, theBall.y, theBall.r * 2);
+}
+
+function mouseReleased() {
+  theBall.dx = (mouseX - theBall.x) * mouseScaling;
+  theBall.dy = (mouseY - theBall.y) * mouseScaling;
+}
+
+function showPath() {
+  if(mouseIsPressed) {
+    fill(0);
+    line(theBall.x, theBall.y, mouseX, mouseY);
+  }
+}
+
+function keyTyped() {
+  if (key === " ") {
+    theBall = spawnBall();
+  }
+}
+
+function draw() {
+  background(220);
+  moveBall();
+  showPath();
+  displayBall();
 }
