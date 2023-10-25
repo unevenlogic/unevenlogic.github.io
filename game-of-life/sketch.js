47e05a786eaf41f34683f15443c0000b8930dad6
unevenlogic <32986800+unevenlogic@users.noreply.github.com>
2023-10-25 12:19:24 -0600

    Added ornaments to game of life
    Colour background now changes based on game state

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -5,14 +5,31 @@ let squareSize = 50;
 const padding = 10;
 let startX;
 let startY;
-const xSize = 40;
-const ySize = 20;
+const xSize = 60;
+const ySize = 30;
 
 let clicked = true;
 let randomSquares = true;
+let continueEvaluation = false;
 let time = 0;
 
 let ms = 0;
+
+let run_ms = 0;
+let no_dim_runtime = 2000;
+let dim_speed = 20;
+
+let start_gol = true;
+let start_ms = 0;
+const start_speed = 10;
+
+const start_text = `Press Enter to evaluate one step
+Press G to continue evaluating
+Press the squares to flip their state
+Press E to empty the grid
+Press R to randomize the grid
+
+Click anywhere to start`;
 
 //const clickRequests = [];
 
@@ -78,6 +95,8 @@ function setup() {
   startX = max(padding, width/2 - squareSize * xSize / 2);
   startY = max(padding, height/2 - squareSize * ySize / 2);
   generateEmptyGrid(grid);
+  background("darkred");
+  window.alert(start_text);
 }
 
 function randomizeGrid(grid) {
@@ -113,19 +132,41 @@ function displayGrid(grid) {
 
 function mousePressed() {
   //clickRequests.push([mouseX, mouseY]);
-  clicked = true;
-  randomSquares = false;
+  if(start_gol) {
+    start_gol = false;
+  }
+  else {
+    clicked = true;
+    randomSquares = false;
+  }
 }
 
 function keyPressed() {
-  if(keyCode === 82) {
-    randomizeGrid(grid);
+  if(start_gol) {
+    start_gol = false;
   }
-  if(keyCode === 69) {
-    generateEmptyGrid(grid);
-  }
-  if(keyCode === ENTER) {
-    grid = evaluateNext();
+  else {
+    randomSquares = false;
+    if(keyCode === 82) {
+      randomSquares = false;
+      continueEvaluation = false;
+      randomizeGrid(grid);
+    }
+    if(keyCode === 69) {
+      randomSquares = false;
+      continueEvaluation = false;
+      generateEmptyGrid(grid);
+    }
+    if(keyCode === ENTER) {
+      randomSquares = false;
+      continueEvaluation = false;
+      grid = evaluateNext();
+    }
+    if(keyCode === 71) {
+      randomSquares = false;
+      continueEvaluation = true;
+      run_ms = millis() + no_dim_runtime;
+    }
   }
 }
 
@@ -137,11 +178,32 @@ function windowResized() {
 }
 
 function draw() {
-  background(50);
-  displayGrid(grid);
-  clicked = false;
-  if(randomSquares && millis() - ms > 100) {
-    randomizeGrid(grid);
-    ms = millis();
+  if(start_gol) {
+    background("darkred");
+    start_ms = millis();
+  }
+  else {
+    if(randomSquares) {
+      background("darkred");
+    }
+    else if(continueEvaluation) {
+      background("darkblue");
+    }
+    else {
+      background("darkgreen");
+    }
+    displayGrid(grid);
+    clicked = false;
+    if(randomSquares && millis() - ms > 100) {
+      randomizeGrid(grid);
+      ms = millis();
+    }
+    else if(continueEvaluation && millis() - ms > 100) {
+      grid = evaluateNext();
+      ms = millis();
+    }
+    if(millis() - start_ms < 255 * start_speed && randomSquares) {
+      background(139, 0, 0, 255 + (start_ms - millis()) / start_speed);
+    }
   }
 }
